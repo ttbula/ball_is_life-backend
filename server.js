9f80842656bc0432a13487d861eff8b60a82a4a8
Timothy Cebula <timothycebula@gmail.com>
2022-05-17 09:38:35 -0400

    Schema added, index && create route added

--- server.js
+++ server.js
@@ -1,32 +1,57 @@
-require("dotenv").config();
+require("dotenv").config()
 
 const { PORT = 3000, MONGODB_URL } = process.env;
-const express = require("express");
+const express = require("express")
 const app = express();
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 // import middlware
 const cors = require("cors");
-const morgan = require("morgan");
+const morgan = require("morgan")
 
 //db connection
 mongoose.connect(MONGODB_URL);
 mongoose.connection
   .on("open", () => console.log("Connected to Mongoose"))
   .on("close", () => console.log("Disconnected"))
-  .on("error", (error) => console.log(error));
+  .on("error", (error) => console.log(error))
+
+//models
+const PlayerSchema = new mongoose.Schema({
+  id: Number,
+  first_name: String,
+  last_name: String
+})
 
 ///////////////////////////////
 // MiddleWare
 ////////////////////////////////
 app.use(cors()); // to prevent cors errors, open access to all origins
-app.use(morgan("dev")); // logging
-app.use(express.json()); // parse json bodies
+app.use(morgan("dev")) // logging
+app.use(express.json()) // parse json bodies
 
 //test route
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("hello world")
 });
 
+//Player index route
+app.get('/player', async (req, res) => {
+  try {
+      res.json(await Player.find({}))
+  } catch (error) {
+      res.status(400).json(error)
+  }
+});
+
+//Player Create
+app.post('/player', async (req,res) => {
+  try {
+    res.json(await Player.create(req.body))
+  } catch (error) {
+    res.status(400).json(error)
+  }
+})
+
 //listener
-app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`))
